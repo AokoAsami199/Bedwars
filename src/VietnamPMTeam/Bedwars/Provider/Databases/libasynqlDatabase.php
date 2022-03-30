944cd5865ce16c7596c360c16756ff973fb4ff41
Ngọc Lam <nglamvn2911@gmail.com>
2022-03-30 11:44:49 +0700

    okay

--- src/VietnamPMTeam/Bedwars/Provider/Databases/libasynqlDatabase.php
+++ src/VietnamPMTeam/Bedwars/Provider/Databases/libasynqlDatabase.php
@@ -4,63 +4,43 @@ declare(strict_types=1);
 
 namespace VietnamPMTeam\Bedwars\Provider\Databases;
 
-use Closure;
+use Generator;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
+use SOFe\AwaitGenerator\Await;
 use VietnamPMTeam\Bedwars\Loader;
 
-class libasynqlDatabase{
+class libasynqlDatabase {
 	protected DataConnector $connector;
 
+	protected const AREAS_INIT = "bedwars.areas.init";
+	protected const AREAS_SELECT_ALL = "bedwars.areas.select.all";
+	protected const AREAS_SELECT_ID = "bedwars.areas.select.id";
+	protected const AREAS_SELECT_DISPLAYNAME = "bedwars.areas.select.displayName";
+	protected const AREAS_SELECT_WORLDNAME = "bedwars.areas.select.worldName";
+	protected const AREAS_REMOVE = "bedwars.areas.remove";
 	protected const AREAS_CREATE = "bedwars.areas.create";
-	protected const AREAS_SELECT = "bedwars.areas.select";
-	protected const AREAS_SELECT_EXIST = "bedwars.areas.selectExists";
+	protected const AREAS_UPDATE_DISPLAYNAME = "bedwars.areas.update.displayName";
 
 	public function __construct(
 		protected Loader $plugin,
 		protected string $sqlType
 	){
+		//NOOP
+	}
+
+	public function load() : void{
 		$sqlMap = [
 			Database::TYPE_MYSQL => Database::SQL . Database::TYPE_MYSQL . ".sql",
 			Database::TYPE_SQLITE => Database::SQL . Database::TYPE_SQLITE . ".sql",
 		];
-		$this->connector = libasynql::create($plugin, [], $sqlMap);
-		$this->connector->executeGeneric(self::AREAS_CREATE);
+		$this->connector = libasynql::create($this->plugin, [], $sqlMap);
+		$this->connector->executeGeneric(self::AREAS_INIT);
 	}
 
-
-
-	public function oldload(Closure $callback) : void{
-		$this->connector->executeSelect(
-			"bedwars." . $this->tableName . ".select",
-			[],
-			function(array $rows) use ($callback){
-				foreach($rows as $result){
-					$callback($result["identifier"], $result);
-				}
-			}
-		);
-	}
-
-	public function oldsave(string $identifier, array $data) : void{
-		$identifierData = ["identifier" => $identifier];
-		$this->connector->executeSelect(
-			"bedwars." . $this->tableName . ".selectExists",
-			$identifierData,
-			function(array $rows) use ($data, $identifierData){
-				if(count($rows) > 0){
-					$this->connector->executeChange(
-						"bedwars." . $this->tableName . ".update",
-						$data,
-					);
-				}
-				$data = array_merge($data, $identifierData);
-				$this->connector->executeInsert(
-					"bedwars." . $this->tableName . ".insert",
-					$data
-				);
-			}
-		);
+	public function asyncSelect(string $query, array $args = []) : Generator{
+		$this->connector->executeSelect($query, $args, yield, yield Await::REJECT);
+		return yield Await::ONCE;
 	}
 
 	public function close() : void{
